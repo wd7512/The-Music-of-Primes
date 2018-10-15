d273dcb928f3e69d96edbc2ac5125d9249501833
wd7512 <35834979+wd7512@users.noreply.github.com>
2018-10-15 21:28:14 +0100

    Version Update

---
+++ V2_Expand/Frequency of Differances - Even.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import math
+f=open('PlistV2.txt','r')
+maxx=len(f.readlines())
+num=int(input('How many primes do we want to plot up to?\nthe max is '+str(maxx)+' type 0 for max\n:'))
+f.close()
+a=0
+if num==0:
+    num=maxx-1
+f=open('PlistV2.txt','r')
+data=[]
+num=num+1
+while a!=num:# recieves data in str form
+    data.append(f.readline())
+    a=a+1
+f.close()
+plots=[]
+for i in range(len(data)):
+    plots.append(i)
+for i in range(len(data)):# converts the list items into integers
+    plots[i] = int(data[i])-int(data[i-1])
+counta=[]
+del plots[0]
+for i in plots:
+    if i>(len(counta)-1):
+        a=0
+        b=i-len(counta)+1
+        while a!=b:
+            a=a+1
+            counta.append(0)
+    counta[i]=counta[i]+1
+for i in range(math.floor(len(counta)/2)):
+    del counta[i+1]
+plt.plot(counta)
+plt.ylabel('prime numbers differance')
+plt.show()
---
+++ V3_Clean/$Gauss' Estimation Plot.py
@@ -0,0 +1,19 @@
+import math
+import time
+n=int(input('Gauss Estimate of how many primes there are up to\nn:'))
+a=2#First Prime
+b=0#Counter
+ty=input('Do you want to see the addition?\ny/n')
+if ty!='y' and ty!='n':
+        print('invalid input')
+elif ty=='y':
+    while a!=n:
+        b=b+1/math.log(a)#Log Function
+        a=a+1
+        print(b)
+elif ty=='n':
+    while a!=n:
+        b=b+1/math.log(a)#Log Function
+        a=a+1
+print(str(b)+'which means there is a '+str(n/b)+'%\nof a prime number up to '+str(n))
+time.sleep(20)
