7463b56a4117b5b6be21dd232f9de2e83b2ba30e
wd7512 <35834979+wd7512@users.noreply.github.com>
2018-10-21 20:55:46 +0100

    a

--- Factorisation/Factor V2/Prime Factor - Frequency V2 Auto - Percent - Filler.py
+++ Factorisation/Factor V2/Prime Factor - Frequency V2 Auto - Percent - Filler.py
@@ -11,25 +11,30 @@ n=int(input('How many numbers do you want to go up to?\n0 for max\nn:'))
 if n==0:
     n=len(y)
 acc=int(input('What percent range do you want\nThe larger the smoother but less accurate'))
-print('Please list the \'number of factors\' lines\nyou would like to draw\ntype 0 when done')
 axi=[]
-ax=int(input(':'))
-while ax!=0:
-    axi.append(ax)
-    ax=int(input(':'))
-print(axi)
-def y2(r):
+for i in range(max(y)):
+    axi.append(i)
+def y1(r):
     a=0
-    y2=[]
+    y1=[]
     for i in range(n):
         if y[i]==r:
             a=a+1
         if i%acc==0:
-            y2.append(a/acc)
+            y1.append(a/acc)
             a=0
+    return(y1)
+def y2(s):
+    y2=y1(s)
+    for i in range(s-1):
+        y2=[a + b for a, b in zip(y2, y1(i+1))]
     return(y2)
+x=[]
+for i in range(len(y2(0))):
+    x.append(100*(i-1))
 for i in range(len(axi)):
     plt.plot(y2(axi[i]), label=str(axi[i])+' Factors')
+    plt.fill_between(x, y2(axi[i]-1), y2(axi[i]))
 plt.xlabel('Number of Ranges')
 plt.ylabel('Percent')
 plt.grid(True)
