179f99e0fbe5a475409a0ad32c08c139bdc70691
wd7512 <35834979+wd7512@users.noreply.github.com>
2018-11-05 07:18:36 +0000

    a

--- Pythagorean Triples/Positive Plot CustomV2.py
+++ Pythagorean Triples/Positive Plot CustomV2.py
@@ -1,13 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from decimal import *
 length=int(input('Bounds?\n:'))
 power=float(input('Power?\n:'))
+acc=input('Accuracy?(<1)\n:')
 exp=[1]
 for i in range(length+1):#i=x
     if round(100*i/(length+1))!=round(100*(i-1)/(length+1)):
         print(str(round(100*i/(length+1)))+'% calculated')
     for j in range(length+1):#j=y
-        if ((i**power+j**power)**(1/power))%1==0 and i!=0 and j!=0:
+        #print(str(i**power)+'+'+str(j**power)+')^'+str(1/power)+'%1='+str(Decimal((i**power+j**power)**(1/power)).quantize(Decimal('0.0000001'))%1))
+        if Decimal((i**power+j**power)**(1/power)).quantize(Decimal(acc))%1==0 and i!=0 and j!=0:
             plt.plot(i,j, 'ro')
             if i==j:#function e.g x[i]=y[i]
                 if exp[-1]!=i:
