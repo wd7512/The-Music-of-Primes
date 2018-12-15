d7f43dd2fabd4e9220a6299c5fe722014e934fdf
wd7512 <wwdennis.home@gmail.com>
2018-12-15 18:07:12 +0000

    a

---
+++ Casio/Poker/Sim2.py
@@ -0,0 +1,57 @@
+import random
+global deck
+
+def newdeck():
+    global deck
+    deck=[]
+    
+    values=[2,3,4,5,6,7,8,9,10,11,12,13,14]
+    suits=['D','S','C','H']
+    
+    for suit in suits:
+        for value in values:
+            deck.append([value,suit])
+    
+    deck=random.sample(deck,52) #shuffle deck
+
+def drawcard():
+    global deck
+    
+    card=deck[0] #taking top card
+    deck.remove(card)
+
+    return card
+
+def deal(players):
+    
+    hands=[]
+    
+    for i in range(players): #first round of dealing
+        hands.append([drawcard()])
+
+    for i in range(players): #second round of dealing
+        hands[i].append(drawcard())
+    
+    return hands
+
+def playtable():
+
+    table=[]
+
+    #flop
+    drawcard() #burning card
+    table.append(drawcard())
+    table.append(drawcard())
+    table.append(drawcard())
+
+    #turn
+    drawcard() #buring card
+    table.append(drawcard())
+
+    #river
+    drawcard() #buring card
+    table.append(drawcard())
+
+    return table
+
+
