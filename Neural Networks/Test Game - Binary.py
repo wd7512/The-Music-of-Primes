7df274f3532861644ce95474ea8bdce617c80b81
wd7512 <wwdennis.home@gmail.com>
2019-02-18 09:41:46 +0000

    a

--- Neural Networks/Test Game - Binary.py
+++ Neural Networks/Test Game - Binary.py
@@ -23,11 +23,69 @@ def randommatrix(low,high,x,y):
             matrix[i,j]=random.randint(low,high)
     return matrix
 
+def converter(num):
+    output=emptymatrix(4,1)
+    if num==0:
+        return output
+    if num%2!=0:
+        output[0,3]=1
+    if num>13:
+        output[0,0]=1
+        output[0,1]=1
+        output[0,2]=1
+        return output
+    if num>11:
+        output[0,0]=1
+        output[0,1]=1
+        return output
+    if num>9:
+        output[0,0]=1
+        output[0,2]=1
+        return output
+    if num>7:
+        output[0,0]=1
+        return output
+    if num>5:
+        output[0,1]=1
+        output[0,2]=1
+        return output
+    if num>3:
+        output[0,1]=1
+        return output
+    if num>1:
+        output[0,2]=1
+        return output
+    return output
+
+def savematrix(matrix,pretext): 
+    count=0
+    for sub in matrix:
+        print(sub)
+        f=open(str(pretext)+'Layer'+str(count)+'.txt','w')
+        save=[]
+        for layer in sub:
+            save.append(str(layer))
+        for i in range(len(save)):
+            save[i]=save[i].split(' ')
+       
+        for s1 in save:
+            s1=[x for x in s1
+                if len(x)>0 and (str.isdigit(x[-1])==True
+                                 or x[-2:]==']]')]
+            if s1[0][0:2]=='[[':
+                s1[0]=s1[0][2:]
+            if s1[-1][-2:]==']]':
+                s1[-1]=s1[-1][:-2]
+            f.write(str(s1)+'\n')
+        f.close()
+        print('saved')
+        count=count+1
+    
+
 def play(test,brain):
     score=0
     for key in test:
-        inp=emptymatrix(4,1)
-        inp[0,key-1]=1
+        inp=converter(key)
         output=inp*brain
         #print(key)
         
@@ -37,7 +95,7 @@ def play(test,brain):
                 output[0,i]=maxx
         for i in range(output.shape[1]):
             if output[0,i]==maxx:
-                num=i+1
+                num=i
         #print(output)
         #print(num)
                 
@@ -49,11 +107,11 @@ def play(test,brain):
 def start(popsize,rounds):
     brains=[]
     for i in range(popsize):
-        brains.append(randommatrix(-100,100,4,4))
+        brains.append(randommatrix(-100,100,16,4))
     test=[]
     for i in range(rounds):
-        test.append(random.randint(1,4))
-    halfbrains=learn(brains,popsize,rounds,test)
+        test.append(random.randint(0,15))
+    halfbrains=learn(brains,popsize,rounds,test,1)
     i=1
     bestscore=0
     xgen=[]
@@ -66,13 +124,15 @@ def start(popsize,rounds):
         #print(len(newbrains))
         test=[]
         for j in range(rounds):
-            test.append(random.randint(1,4))
-        halfbrains=learn(newbrains,popsize,rounds,test)
+            test.append(random.randint(0,15))
+        halfbrains=learn(newbrains,popsize,rounds,test,i)
         bestscore=play(test,halfbrains[0])
         print(bestscore)
         xgen.append(i)
         yscore.append(bestscore)
         i=i+1
+
+    savematrix(halfbrains[0],'binary1')
 
     plt.plot(xgen,yscore)
     plt.show()
@@ -82,18 +142,19 @@ def start(popsize,rounds):
         
     
         
-def learn(brains,popsize,rounds,test):
+def learn(brains,popsize,rounds,test,gen):
     scores=[]
     
     for i in range(popsize):
         
         score=play(test,brains[i])
         scores.append([score*100,i+1])
+    #print(scores)
     print(max(scores))
     #print(scores)
     live=[]
     for score in scores:
-        if random.randint(0,100)<score[0]:
+        if random.randint(0,int(gen))<score[0]:
             live.append(score)
 
     #print(live)
@@ -104,9 +165,9 @@ def learn(brains,popsize,rounds,test):
         livebrains.append(brains[life[1]-1])
     #print(livebrains)
     #print(len(livebrains))
-    while len(livebrains)!=popsize/4: #takes top 25%
+    while len(livebrains)!=popsize/4: #takes top %
         if len(livebrains)<popsize/4:
-            livebrains.append(randommatrix(-100,100,4,4))
+            livebrains.append(randommatrix(-100,100,16,4))
         else:
             livebrains=livebrains[0:len(livebrains)-1]
     return livebrains
@@ -116,8 +177,10 @@ def evolve(halfbrains):
     brains=halfbrains[:]
     for i in range(4):
         for j in range(index):
-            for k in range(3*(4-i)): #increased pop by 300% to maintain
-                add=randommatrix(-6,6,4,4)
+            for k in range(3*(4-i)): #increased pop to maintain
+                add=randommatrix(-10,10,16,4)
                 brains.append(add+brains[i*index+j])
 
     return brains
+
+start(2000,100)
