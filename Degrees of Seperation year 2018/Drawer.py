370c04f0df0ec8097b068ab187535d369f5f3faa
wd7512 <wwdennis.home@gmail.com>
2019-02-28 18:26:02 +0000

    a

--- Degrees of Seperation year 2018/Drawer.py
+++ Degrees of Seperation year 2018/Drawer.py
@@ -1,6 +1,7 @@
 import turtle
 import os
 import math
+import random
 files=os.listdir()
 for filename in files:
     if (filename[-3:])!='txt':
@@ -16,31 +17,51 @@ for filename in files:
 
 nosubnodes=len(files)
 nomainnodes=len(mainnodes)
-print(str(nosubnodes)+' subnodes')
+print(str(nosubnodes)+' nodes')
 print(str(nomainnodes)+' mainnodes')
 
-radius=360
+radius=380
 pen=turtle.Turtle()
-pen.speed(1000)
+pen.speed(10000000)
+pen.ht()
 #pen.penup()
 #pen.setpos(0,-radius)
 #pen.pendown()
 #pen.circle(radius)
 angle=2*math.pi/nosubnodes
 
+points=[]
 
 coords=[]
 minirad=10
 for i in range(nosubnodes):
+    points.append(0)
     pen.penup()
-    x=math.sin(angle*i)*radius
-    y=math.cos(angle*i)*radius
+
+    if files[i] in mainnodes:
+        pen.color('blue')
+        x=math.sin(angle*i)*(radius)
+        y=math.cos(angle*i)*(radius)
+    else:
+        pen.color('orange')
+        x=math.sin(angle*i)*radius
+        y=math.cos(angle*i)*radius
+    
+    
+    #x=random.randint(-radius,radius)
+    #y=random.randint(-radius,radius)
+    
     coords.append([x,y])
     pen.setpos(x,y-minirad)
     pen.pendown()
     
+    
+    
+    
     pen.circle(minirad)
 
+
+pen.color('Black')
 for node in mainnodes:
     pos=coords[files.index(node)]
     
@@ -56,5 +77,13 @@ for node in mainnodes:
         pen.setpos(pos[0],pos[1])
         pen.pendown()
         pen.setpos(moveto[0],moveto[1])
+
+        points[coords.index(pos)]=points[coords.index(pos)]+1
+        points[coords.index(moveto)]=points[coords.index(moveto)]+1
+        
     f.close()
     
+for i in range(nosubnodes):
+    points[i]=[points[i],files[i]]
+
+
