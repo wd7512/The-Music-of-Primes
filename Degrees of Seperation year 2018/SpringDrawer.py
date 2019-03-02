2362843f0ef6778deaf5feed755f6481aa3ffda9
wd7512 <wwdennis.home@gmail.com>
2019-03-02 17:52:37 +0000

    Update SpringDrawer.py

--- Degrees of Seperation year 2018/SpringDrawer.py
+++ Degrees of Seperation year 2018/SpringDrawer.py
@@ -93,21 +93,33 @@ coords=[]
 minirad=10
 freqa=[]
 for i in range(nosubnodes):
-    pen.penup()
-
-
-
+    
     close=sets[i][0]
     #print(close)
     freqa.append(close)
-
     
-
-
-    
-    close=int(1.92**(close))
     #print(close)
 
+
+
+
+freq=[0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+for re in freqa:
+    #print(re)
+    freq[re]=freq[re]+1
+countvar=0
+for fre in freq:
+    print(str(fre)+' people with '+str(countvar)+' unique connections')
+    countvar=countvar+1
+#print(freq)
+
+
+for i in range(nosubnodes):
+    pen.penup()
+
+    close=int(1.92**(freqa[i]))
+    
     if files[i] in mainnodes:
         pen.color('blue')
         x=math.sin(angle*i)*(radius-close)
@@ -130,37 +142,84 @@ for i in range(nosubnodes):
     
     pen.circle(minirad)
 
-freq=[0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-for re in freqa:
-    #print(re)
-    freq[re]=freq[re]+1
-countvar=0
-for fre in freq:
-    print(str(fre)+' people with '+str(countvar)+' unique connections')
-    countvar=countvar+1
-#print(freq)
+def drawcircles(minirad,files,mainnodes):
 
-
-pen.color('Black')
-for node in mainnodes:
-    pos=coords[files.index(node)]
-    
-    f=open(node,'r')
-    names=f.readlines()
-    for name in names:
-        name=name[0:-1]+'.txt'
-        index=files.index(name)
-        moveto=coords[index]
-
-
+    for i in range(len(files)):
         pen.penup()
-        pen.setpos(pos[0],pos[1])
+        pos=coords[i]
+        if files[i] in mainnodes:
+            pen.color('blue')
+        else:
+            pen.color('orange')
+
+        x=pos[0]
+        y=pos[1]
+        pen.setpos(x,y-minirad)
         pen.pendown()
-        pen.setpos(moveto[0],moveto[1])
-
-
-        
-    f.close()
+        pen.circle(minirad)
     
 
+def drawlines(mainnodes,files,coords):
+
+    pen.color('Black')
+    for node in mainnodes:
+        pos=coords[files.index(node)]
+        
+        f=open(node,'r')
+        names=f.readlines()
+        for name in names:
+            name=name[0:-1]+'.txt'
+            index=files.index(name)
+            moveto=coords[index]
+
+
+            pen.penup()
+            pen.setpos(pos[0],pos[1])
+            pen.pendown()
+            pen.setpos(moveto[0],moveto[1])
+
+
+            
+        f.close()
+
+
+
+def shiftcoord(coords,files,sets):
+
+    tension=0.05
+    springsize=40
+    for i in range(len(files)):
+        pos1=coords[i]
+        x1=pos1[0]
+        y1=pos1[1]
+        peeps=sets[i][2]
+
+        vectors=[]
+        for peep in peeps:
+            pos2=coords[peep]
+            x2=pos2[0]
+            y2=pos2[1]
+            distance=math.sqrt((x2-x1)**2+(y2-y1)**2)
+            grad=(y2-y1)/(x2-x1)
+            ang=math.atan(grad)
+            vector=[math.cos(ang)*tension*distance,
+                    math.sin(ang)*tension*distance]
+            vectors.append(vector)
+
+
+        for vector in vectors:
+            x1=x1+vector[0]
+            y1=y1+vector[1]
+
+        print([x1,y1])
+
+        coords[i]=[x1,y1]
+
+    return coords
+               
+drawlines(mainnodes,files,coords)
+coords=shiftcoord(coords,files,sets)
+pen.clear()
+drawcircles(minirad,files,mainnodes)
+drawlines(mainnodes,files,coords)
