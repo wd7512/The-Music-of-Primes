8fe804fba16db3ae468a71601bc1334890b51a5e
wd7512 <wwdennis.home@gmail.com>
2019-09-06 23:20:35 +0100

    Update Connect 4.py

--- Connect 4/Connect 4.py
+++ Connect 4/Connect 4.py
@@ -19,4 +19,56 @@ def addchip(playernum,col,game):
     return game
 
 def check_four(game):
-    print('checks if 4')
+    
+    
+    for i in range(6): #horizontal
+        line = game[i]
+        for j in range(4):
+            if line[j] != 0 and all(x == line[j] for x in line[j:j+4]) == True:
+                return line[j]
+
+    for i in range(7):
+        line = [game[0][i],game[1][i],game[2][i],game[3][i],game[4][i],game[5][i]]
+        for j in range(3):
+            if line[j] != 0 and all(x == line[j] for x in line[j:j+4]) == True:
+                return line[j]
+
+    for i in range(3):
+        for j in range(4):
+            line = [game[i][j],game[i+1][j+1],game[i+2][j+2],game[i+3][j+3]]
+            if line[0] != 0 and all(x == line[0] for x in line) == True:
+                return line[0]
+
+    for i in range(3):
+        for j in range(4):
+            line = [game[i+3][j],game[i+2][j+1],game[i+1][j+2],game[i][j+3]]
+            if line[0] != 0 and all(x == line[0] for x in line) == True:
+                return line[0]
+
+    return False
+
+def run_human():
+    game = newgame()
+
+    turn = 1
+    
+    while check_four(game) == False:
+        print(game)
+        col = int(input('p'+str(turn)+' column:'))
+        addchip(turn,col,game)
+        if turn == 1:
+            turn = 2
+        else:
+            turn = 1
+
+    if turn == 1:
+        turn = 2
+    else:
+        turn = 1
+    print('player '+str(turn)+' wins')
+    print(game)
+
+def input_convert(game):
+    return game.resize((1,42))
+def input_revert(game):
+    return game.resize((6,7))
