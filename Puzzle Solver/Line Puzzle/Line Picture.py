7de1ef982816b3ecf7cf96466385317f0b2cbae4
wd7512 <35834979+wd7512@users.noreply.github.com>
2019-12-02 02:57:18 +0000

    a

--- Puzzle Solver/Line Puzzle/Line Picture.py
+++ Puzzle Solver/Line Puzzle/Line Picture.py
@@ -1,6 +1,9 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+from google_images_download import google_images_download
+import os
 
 def random_picture(length,size):
     picture = []
@@ -95,4 +98,51 @@ def rearrange_decoder(picture,output): # decodes and rearranges pictue, given th
 
     return newpicture
 
-        
+def grey(picture_name):
+    img = Image.open(picture_name).convert('LA')
+    print('dim - '+str(np.size(img)))
+    name = picture_name+'-grey.png'
+    print(name+' saved')
+    img.save(name)
+
+def downloadimages(query):
+    response = google_images_download.googleimagesdownload()  
+    # keywords is the search query 
+    # format is the image file format 
+    # limit is the number of images to be downloaded 
+    # print urs is to print the image file url 
+    # size is the image size which can 
+    # be specified manually ("large, medium, icon") 
+    # aspect ratio denotes the height width ratio 
+    # of images to download. ("tall, square, wide, panoramic") 
+    arguments = {"keywords": query, 
+                 "format": "jpg", 
+                 "limit":100, 
+                 "print_urls":True, 
+                 "size": "large", 
+                 "aspect_ratio": "panoramic"}
+    try: 
+        response.download(arguments) 
+      
+    # Handling File NotFound Error     
+    except FileNotFoundError:  
+        arguments = {"keywords": query, 
+                     "format": "jpg", 
+                     "limit":4, 
+                     "print_urls":True,  
+                     "size": "medium"} 
+                       
+        # Providing arguments for the searched query 
+        try: 
+            # Downloading the photos based 
+            # on the given arguments 
+            response.download(arguments)  
+        except: 
+            pass
+
+def grey_folder(folder):
+    images = [name for name in os.listdir(folder)]
+    print(images)
+    for im in images:
+        newname = folder + '/' +im
+        grey(newname)
