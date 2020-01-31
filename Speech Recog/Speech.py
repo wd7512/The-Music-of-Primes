c767a72ac82858815f713c29d8b9087f60c1200a
wd7512 <wwdennis.home@gmail.com>
2020-01-31 13:36:15 +0000

    a

---
+++ Speech Recog/Speech.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+import sounddevice as sd
+from scipy.io import wavfile
+import numpy as np
+
+
+mic = sr.Microphone()
+
+r = sr.Recognizer()
+
+with mic as source:
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source)
+try:
+    print(r.recognize_google(audio))
+except sr.RequestError:
+    print('Connection to API failed')
+    
+except sr.UnknownValueError:
+    print('Unable to recognize speech')
