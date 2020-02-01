1445f70a56199a7705e02efefe1702f4c3d93154
wd7512 <wwdennis.home@gmail.com>
2020-02-01 18:16:58 +0000

    Update Speech.py

--- Speech Recog/Speech.py
+++ Speech Recog/Speech.py
@@ -2,20 +2,15 @@ import speech_recognition as sr
 import sounddevice as sd
 from scipy.io import wavfile
 import numpy as np
+import time
 
 #r.recognize_google(audio, language="fr-FR") french example
 
 
-r = sr.Recognizer()
 
-'''
-mic = sr.Microphone()
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
-'''
 
-def convert_speech(source):
+def convert_speech(audio):
+    r = sr.Recognizer()
 
     output = False
     
@@ -29,10 +24,34 @@ def convert_speech(source):
         print('Unable to recognize speech')
 
     return output    
+
+def existing_audio(filename):
+    r = sr.Recognizer()
+    audio_file = sr.AudioFile(filename)
+    with audio_file as source:
+        audio = r.record(source)
+
+    return audio
+
+def using_mic():
+
+    r = sr.Recognizer()
+    mic = sr.Microphone()
     
-audio_file = sr.AudioFile('basic.wav')
-with audio_file as source:
-    audio = r.record(source)
+    countdown = 3
+    for i in range(countdown):
+        print('Speak in '+str(countdown-i))
+        time.sleep(1)
+    print('Mic on')
+    
+    
+    with mic as source:
+        #r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    print('Mic off')
+
+    return audio
 
 
-
+convert_speech(using_mic())
