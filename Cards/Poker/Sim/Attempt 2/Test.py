e10be06378f9aa1c08e0a6cc52d9296eac3c1ed3
wd7512 <35834979+wd7512@users.noreply.github.com>
2020-02-12 20:46:49 +0000

    Update Test.py

--- Cards/Poker/Sim/Attempt 2/Test.py
+++ Cards/Poker/Sim/Attempt 2/Test.py
@@ -1,12 +1,54 @@
 import random
 import os
 import ast
+import tkinter
 allfiles = os.listdir()
 filenames = []
 for file in allfiles:
     if file[0] == '[':
         #print(file)
         filenames.append(file)
+
+
+
+def conv(filename):
+    brek = filename.index(']')
+    if filename[brek+1] == 0:
+        sui = 'suited'
+    else:
+        sui = 'off-suit'
+
+    name = filename[1:brek]
+    if file[-5:] == 'r.txt':
+        end = 'river'
+    else:
+        end = 'flop'
+
+    return name +' '+ sui +' '+end
+'''
+score = 0
+rounds = int(input('No. of Rounds:'))
+for i in range(rounds):
+    file = random.choice(testfiles)
+    f = open(file,'r')
+    rawdata = f.readlines()
+    f.close()
+    data = []
+    for a in rawdata:
+        data.append(ast.literal_eval(a))
+    cata = random.choice(possibles)
+    for a in data:
+        if a[1] == cata:
+            test = a
+    print(conv(file))
+    ans = float(input(cata+' : '))
+    print(str(test)+'\n')
+    score = score + abs(test[0]-ans)
+
+print(score/rounds)
+'''
+window = tkinter.Tk()
+window.title('Poker Test')
 
 possibles = []
 
@@ -19,6 +61,19 @@ Flush = True
 Full = True
 Quads = True
 Straight_Flush = True
+
+tkinter.Checkbutton(window, text = 'High Card',variable = High,onvalue = True, offvalue=False).grid(row=0)
+tkinter.Checkbutton(window, text = 'One Pair',variable = One,onvalue = True, offvalue=False).grid(row=1)
+tkinter.Checkbutton(window, text = 'Two Pair',variable = Two,onvalue = True, offvalue=False).grid(row=2)
+tkinter.Checkbutton(window, text = 'Three of a Kind',variable = Three,onvalue = True, offvalue=False).grid(row=3)
+tkinter.Checkbutton(window, text = 'Straight',variable = Straight,onvalue = True, offvalue=False).grid(row=4)
+tkinter.Checkbutton(window, text = 'Flush',variable = Flush,onvalue = True, offvalue=False).grid(row=5)
+tkinter.Checkbutton(window, text = 'Full House',variable = Full,onvalue = True, offvalue=False).grid(row=6)
+tkinter.Checkbutton(window, text = 'Quads',variable = Quads,onvalue = True, offvalue=False).grid(row=7)
+tkinter.Checkbutton(window, text = 'Straight Flush',variable = Straight_Flush,onvalue = True, offvalue=False).grid(row=8)
+
+
+
 
 if High == True:
     possibles.append('High Card')
@@ -53,34 +108,3 @@ if river == True:
             testfiles.append(file)
     
 
-def conv(filename):
-    brek = filename.index(']')
-    if filename[brek+1] == 0:
-        sui = 'suited'
-    else:
-        sui = 'off-suit'
-
-    name = filename[1:brek]
-    if file[-5:] == 'r.txt':
-        end = 'river'
-    else:
-        end = 'flop'
-
-    return name +' '+ sui +' '+end
-
-
-while True:
-    file = random.choice(testfiles)
-    f = open(file,'r')
-    rawdata = f.readlines()
-    f.close()
-    data = []
-    for a in rawdata:
-        data.append(ast.literal_eval(a))
-    cata = random.choice(possibles)
-    for a in data:
-        if a[1] == cata:
-            test = a
-    print(conv(file))
-    ans = float(input(cata+' : '))
-    print(str(test)+'\n')
