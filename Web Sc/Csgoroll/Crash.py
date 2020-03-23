36d22c8c1e13ba76bda86404e44ccdfc23f95ca7
wd7512 <35834979+wd7512@users.noreply.github.com>
2020-03-23 14:30:36 +0000

    a

---
+++ Web Sc/Csgoroll/Crash.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def add_data():
+    while True:
+        num = float(input('Number: '))
+        f = open('Crash Data.txt','a')
+        f.write(str(num)+'\n')
+        f.close()
+
+def show_data():
+    
+    data = get_data()
+    plt.hist(data,density = True)
+    plt.show()
+
+def get_data():
+    f = open('Crash Data.txt','r')
+    data = f.readlines()
+    f.close()
+    data = [float(d) for d in data]
+
+    return data
+    
+def ana_data():
+    bet = 1
+    values = np.arange(1,10,0.1).tolist()
+    profit_list = []
+    data = get_data()
+    tot = len(data)
+    win_freqs = []
+    for val in values:
+        profit = 0
+        win_freq = 0
+        for point in data:
+            if val < point:
+                win_freq = win_freq + 1
+
+        win_freq = win_freq / tot
+        profit = win_freq * (bet * (val - 1)) - bet * (1 - win_freq)
+        
+        profit_list.append(profit)
+        win_freqs.append(win_freq)
+
+    
+    plt.plot(values,win_freqs)
+    #plt.plot(values,profit_list)
+    plt.show()
+
+
+
+
