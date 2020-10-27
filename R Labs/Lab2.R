40048add9730ab3c669c833511386f19c7dbeb21
wd7512 <35834979+wd7512@users.noreply.github.com>
2020-10-27 10:53:25 +0000

    Update Lab2.R

--- R Labs/Lab2.R
+++ R Labs/Lab2.R
@@ -5,3 +5,22 @@ n = 100
 mu = 0
 sigma = 1
 momArray = array(0, dim=c(nSim,2))
+
+for (i in 1:nSim){
+  #simulate values from log-normal
+  y <- exp(rnorm(n=n, mean=mu, sd=sigma))
+  
+  #calculate mom estimates and save them into the array
+  m1 <- mean(y)
+  m2 <- mean(y^2)
+  mom_mu <- 2*log(m1)-0.5*log(m2)
+  mom_sigmasq <- log(m2) - 2*log(m1)
+  momArray[i,] <- c(mom_mu, mom_sigmasq)
+  
+}
+
+hist(momArray[,1], main = expression('Distribution MOM estimator of'~mu),
+     xlab = expression(hat(mu)))
+
+hist(momArray[,2], main = expression('Distribution MOM estimator of'~sigma^2),
+     xlab = expression(widehat(sigma^2)))
