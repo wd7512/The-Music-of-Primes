bd39e0d113551285a4025f8e4384c38deffb938a
wd7512 <35834979+wd7512@users.noreply.github.com>
2020-11-04 10:44:51 +0000

    a

--- Steam API/Steam.py
+++ Steam API/Steam.py
@@ -6,7 +6,7 @@ from datetime import datetime
 #print(pd.__version__)
 #https://csgobackpack.net/api/
 
-
+inv_id = 76561198162512099
 
 #https://steamcommunity.com/market/search/render/?search_descriptions=0&sort_column=default&sort_dir=desc&appid=730&norender=1&count=9999999999
 #gets all csgo items
@@ -26,9 +26,26 @@ def run_api():
         f.write(text)
 
     
+def open_data():
+
+
+    data = json.load(open('TEMP.json'))
+    df = pd.DataFrame(data["items_list"])
+
+    return df
+
+def open_inv(inv_id):
+
+    url = 'http://csgobackpack.net/api/GetInventoryValue/?id=' +str(inv_id)
+
+    response = requests.get(url)
+    print(response.status_code)    
+
+    with open('TEMP.json','w') as f:
+        text = json.dumps(response.json(), sort_keys=True, indent=4)
+        f.write(text)
 
 
 
-
-
-run_api()
+#run_api()
+open_inv(inv_id)
