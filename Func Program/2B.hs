4c00278816b72fc2dd40715cddc2def9f6d2f354
wd7512 <wwdennis.home@gmail.com>
2021-03-19 16:09:49 +0000

    a

--- Func Program/2B.hs
+++ Func Program/2B.hs
@@ -10,3 +10,67 @@ at :: [a] -> Int -> a
 at (x:_)  0 = x
 at (_:xs) n = at xs (n-1)
 
+find :: Eq a => a -> [(a,b)] -> b
+find _ [] = error "item not found"
+find x ((y,z):zs)
+    | x == y    = z
+    | otherwise = find x zs 
+
+which :: Eq a => a -> [a] -> Int
+which = aux 0
+    where
+        aux :: Eq a => Int -> a -> [a] -> Int
+        aux _ x [] = error "item not found"
+        aux i x (y:ys)
+            | x == y    = i
+            | otherwise = aux (i+1) x ys
+
+
+
+member :: Eq a => [a] -> a -> Bool
+member [] _     = False
+member (x:xs) y
+    | x == y    = True 
+    | otherwise = member xs y
+
+remove :: Eq a => [a] -> a -> [a]
+remove [] _     = []
+remove (x:xs) y
+    | x == y    = xs
+    | otherwise = x : remove xs y
+
+before :: Ord a => [a] -> [a] -> Bool 
+before _ [] = False
+before [] _ = True
+before (x:xs) (y:ys)
+    | x < y     = True 
+    | x == y    = before xs ys
+    | otherwise = False
+
+
+sorted :: Ord a => [a] -> Bool 
+sorted [] = True 
+sorted [x]  = True
+sorted (x:y:zs) = x < y && sorted (y:zs)
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+    | x < y     = x : merge xs (y:ys)
+    | x > y     = y : merge (x:xs) ys
+    | otherwise = x : merge xs ys
+
+minus :: Ord a => [a] -> [a] -> [a]
+minus xs [] = xs
+minus [] ys = []
+minus xs (y:ys)
+    | member xs y = minus (remove xs y) ys
+    | otherwise   = minus xs ys
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort (take n xs)) (msort (drop n xs))
+    where
+        n = div (length xs) 2
--- Func Program/6A.hs
+++ Func Program/6A.hs
@@ -1,4 +1,4 @@
-
+{-
 data IntTree = Empty | Node Int IntTree IntTree
 
 t :: IntTree
@@ -16,35 +16,10 @@ instance Show IntTree where
 isEmpty :: IntTree -> Bool
 isEmpty Empty = True
 isEmpty _     = False
+-}
 
-------------------------- Exercise 1
+data Tree a = Empty | Node a (Tree a) (Tree a)
 
-member :: Int -> IntTree -> Bool
-member _ Empty               = False
-member i (Node x left right) = member i left || member i right || x == i
-
-largest :: IntTree -> Int
-largest Empty            = 0
-largest (Node x l Empty) = x
-largest (Node x l r)     = largest r
-
-ordered :: IntTree -> Bool
-ordered = undefined
-
-deleteLargest :: IntTree -> IntTree
-deleteLargest = undefined
-
-delete :: Int -> IntTree -> IntTree
-delete _ Empty = undefined
-delete y (Node x l r)
-    | y < x     = undefined
-    | y > x     = undefined
-    | isEmpty l = undefined
-    | otherwise = undefined
-
-------------------------- Exercise 2
-
-{-
 instance Show a => Show (Tree a) where
     show = unlines . aux ' ' ' '
       where
@@ -53,93 +28,46 @@ instance Show a => Show (Tree a) where
           [ c:' ':m | m <- aux ' ' '|' s ] ++ 
           ['+':'-':show x] ++ 
           [ d:' ':n | n <- aux '|' ' ' t ]
--}
+
+t :: Tree Int
+t = Node 4 (Node 2 (Node 1 Empty Empty) (Node 3 Empty Empty)) (Node 5 Empty (Node 6 Empty Empty))
+
+isEmpty :: Tree a -> Bool
+isEmpty Empty = True
+isEmpty _     = False
 
 
+member :: Ord a => a -> Tree a -> Bool
+member _ Empty               = False
+member i (Node x left right) = member i left || member i right || x == i
 
-------------------------- Lambda-calculus
+largest :: Tree a -> a
+largest Empty            = error "largest : empty tree"
+largest (Node x l Empty) = x
+largest (Node x l r)     = largest r
 
-type Var = String
+flatten :: Tree a -> [a]
+flatten Empty = []
+flatten (Node x l r) = flatten l ++ [x] ++ flatten r
 
-data Term =
-    Variable Var
-  | Lambda   Var  Term
-  | Apply    Term Term
+sorted :: Ord a => [a] -> Bool
+sorted [] = True 
+sorted [x]  = True
+sorted (x:y:zs) = x < y && sorted (y:zs)
 
-example :: Term
-example = Lambda "a" (Lambda "x" (Apply (Apply (Lambda "y" (Apply (Variable "a") (Variable "c"))) (Variable "x")) (Variable "b")))
+ordered :: Ord a => Tree a -> Bool
+ordered = sorted . flatten
 
-pretty :: Term -> String
-pretty = f 0
-    where
-      f i (Variable x) = x
-      f i (Lambda x m) = if i /= 0 then "(" ++ s ++ ")" else s where s = "\\" ++ x ++ ". " ++ f 0 m 
-      f i (Apply  n m) = if i == 2 then "(" ++ s ++ ")" else s where s = f 1 n ++ " " ++ f 2 m
+deleteLargest :: Tree a -> Tree a
+deleteLargest Empty            = error "deleteLargest : empty tree"
+deleteLargest (Node x l Empty) = l
+deleteLargest (Node x l r)     = (Node x l (deleteLargest r))
 
-instance Show Term where
-  show = pretty
+delete :: Ord a => a -> Tree a -> Tree a
+delete _ Empty = Empty
+delete y (Node x l r)
+  | y < x     = Node x (delete y l) r
+  | y > x     = Node x l (delete y r)
+  | isEmpty l = r
+  | otherwise = Node (largest l) (deleteLargest l) r
 
-n1 :: Term
-n1 = Lambda "x" (Variable "x")
-
-n2 :: Term
-n2 = Lambda "x" (Apply (Lambda "y" (Variable "x")) (Variable "z"))
-
-n3 :: Term
-n3 = Apply (Lambda "x" (Lambda "y" (Apply (Variable "x") (Variable "y")))) n1
-
--------------------------
-
-merge :: Ord a => [a] -> [a] -> [a]
-merge xs [] = xs
-merge [] ys = ys
-merge (x:xs) (y:ys)
-    | x == y    = x : merge xs ys
-    | x <= y    = x : merge xs (y:ys)
-    | otherwise = y : merge (x:xs) ys
-
-minus :: Ord a => [a] -> [a] -> [a]
-minus xs [] = xs
-minus [] ys = []
-minus (x:xs) (y:ys)
-    | x <  y    = x : minus    xs (y:ys)
-    | x == y    =     minus    xs    ys
-    | otherwise =     minus (x:xs)   ys
-
--------------------------
-
-used :: Term -> [Var]
-used (Variable x) = [x]
-used (Lambda x n) = [x] `merge` used n
-used (Apply  n m) = used n `merge` used m
-
-free :: Term -> [Var]
-free (Variable x) = [x]
-free (Lambda x n) = free n `minus` [x]
-free (Apply  n m) = free n `merge` free m
-
-
-------------------------- Exercise 3
-
-numeral :: Int -> Term
-numeral = undefined
-
-------------------------- Exercise 4
-
-variables :: [Var]
-variables = undefined
-
-fresh :: [Var] -> Var
-fresh = undefined
-
-rename :: Var -> Var -> Term -> Term
-rename x y (Variable z)
-    | z == x    = undefined
-    | otherwise = undefined
-rename x y (Lambda z n)
-    | z == x    = undefined
-    | otherwise = undefined
-rename x y (Apply n m) = undefined
-
-substitute :: Var -> Term -> Term -> Term
-substitute = undefined
