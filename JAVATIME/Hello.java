d63e38390cd49864fb10b85f36cd710dd4b5447f
wd7512 <wwdennis.home@gmail.com>
2021-04-13 16:12:57 +0100

    a

--- JAVATIME/Hello.java
+++ JAVATIME/Hello.java
@@ -1,20 +1,13 @@
+import javax.swing.*;
+class gui{
+    public static void main(String args[]){
+       JFrame frame = new JFrame("THE GUI FIRST");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setSize(300,300);
 
- public class Hello {
-     public static void main(String[] args) {
-         int fib0 = 0;
-         int fib1 = 0;
-         int fib2 = 1;
-         for (int i = 0; i < 100; i++) {
-             fib0 = fib1;
-             fib1 = fib2;
-             fib2 = fib1 + fib0;
-             System.out.println(fib2);
-             if (fib2 < 0) {
-                 System.out.println(" uhh what " + i);
-             }
-         }
-         
-         
-     }
- }
- 
+       
+       JButton button = new JButton("Press");
+       frame.getContentPane().add(button); // Adds Button to content pane of frame
+       frame.setVisible(true);
+    }
+}
