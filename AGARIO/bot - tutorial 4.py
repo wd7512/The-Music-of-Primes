fedefaf59312ae858950d95de675b9cd008ea868
wd7512 <wwdennis.home@gmail.com>
2021-04-22 16:04:12 +0100

    a

--- AGARIO/bot - tutorial 4.py
+++ AGARIO/bot - tutorial 4.py
@@ -1,65 +1,40 @@
 import cv2 as cv
 import pyautogui as pag
-import time
 import numpy as np
+from time import time
+from PIL import ImageGrab
+from vision import findClickPositions
 
-def findClickPositions(needle_img_path, haystack_img_path, threshold = 0.5, debug_mode = None):
+mid_x = 1920/2
+mid_y = 1080/2
+loop_time = time()
+while True:
+    screenshot = ImageGrab.grab()
+    screenshot = np.array(screenshot)
+    screenshot = cv.cvtColor(screenshot,cv.COLOR_RGB2GRAY)
+    
+    #cv.imshow('Computer Vision',screenshot)
 
-    needle_img = cv.imread(needle_img_path,cv.IMREAD_GRAYSCALE)
-    haystack_img = cv.imread(haystack_img_path,cv.IMREAD_GRAYSCALE)
+    pag.moveTo(mid_x,mid_y)
+    foods = findClickPositions('food.png',screenshot,0.8)
 
-    needle_w = needle_img.shape[1]
-    needle_h = needle_img.shape[0]
+    
 
+    if len(foods):
+        closest = foods[0]
+        for loc in foods:
+            if abs(mid_x - loc[0]) + abs(mid_y - loc[1]) < abs(mid_x - closest[0]) + abs(mid_y - closest[1]):
+                closest = loc
 
-    result = cv.matchTemplate(haystack_img,needle_img,cv.TM_CCOEFF_NORMED)
-    #cv.imshow('Result',result)
+        pag.moveTo(closest[0],closest[1])
 
-    locations = np.where(result >= threshold)
-
-    locations = list(zip(*locations[::-1]))
-
-    rectangles = []
-    for loc in locations:
-        rect = [int(loc[0]),int(loc[1]),needle_w,needle_h]
-        rectangles.append(rect)
-        #can repeat line above as grouping removes single rects
-
-    rectangles,weights = cv.groupRectangles(rectangles,1,0.1)
-
-    #print(rectangles)
         
-    points = []
-    if len(rectangles):
-        print('Found needle(s).')
-        
-        
-        line_color = (0,0,0)
-        line_type = cv.LINE_4
+    
+    print('FPS {}'.format(1/(time() - loop_time)))
+    loop_time = time()
 
-        marker_color = (0,0,0)
-        marker_type = cv.MARKER_CROSS
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows
+        break
 
-        for (x,y,w,h) in rectangles:
-
-            center_x = x + int(w/2)
-            center_y = y + int(h/2)
-
-            points.append((center_x,center_y))
-
-            if debug_mode == 'points':
-                cv.drawMarker(haystack_img, (center_x,center_y), marker_color, marker_type)
-            elif debug_mode == 'rectangles':
-                top_left = (x,y)
-                bottom_right = (x + w, y + h)
-
-                cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
-
-
-        if debug_mode:
-            cv.imshow('Matches',haystack_img)
-            cv.waitKey()
-
-    return points
-
-print(findClickPositions('food.png','haystack.png',0.8,'rectangles'))
+print('Done.')
